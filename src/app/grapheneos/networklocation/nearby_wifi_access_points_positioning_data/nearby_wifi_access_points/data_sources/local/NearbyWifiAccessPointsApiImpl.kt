d4a5b28afaa2b27ca7f47418068dd584b15c8db2
soupslurpr <92235850+soupslurpr@users.noreply.github.com>
2024-11-19 16:00:31 -0800

    use suspendCancellableCoroutine for fetchFreshestNearbyWifiAccessPoints instead of while loop with delay

--- src/app/grapheneos/networklocation/nearby_wifi_access_points_positioning_data/nearby_wifi_access_points/data_sources/local/NearbyWifiAccessPointsApiImpl.kt
+++ src/app/grapheneos/networklocation/nearby_wifi_access_points_positioning_data/nearby_wifi_access_points/data_sources/local/NearbyWifiAccessPointsApiImpl.kt
@@ -7,10 +7,12 @@ import android.net.wifi.WifiScanner.ScanListener
 import android.os.SystemClock
 import android.os.WorkSource
 import android.util.Log
+import kotlin.coroutines.resume
 import kotlin.properties.Delegates
 import kotlin.time.Duration.Companion.milliseconds
 import kotlin.time.Duration.Companion.nanoseconds
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.suspendCancellableCoroutine
 
 class NearbyWifiAccessPointsApiImpl(
     private val wifiScanner: WifiScanner,
@@ -23,6 +25,7 @@ class NearbyWifiAccessPointsApiImpl(
         if (!wifiManager.isWifiScannerSupported && !(wifiManager.isWifiEnabled || wifiManager.isScanAlwaysAvailable)) {
             return null
         }
+
         // TODO: can use more time-consuming settings if we have enough time
         val scanSettings = WifiScanner.ScanSettings()
         scanSettings.band = WifiScanner.WIFI_BAND_5_GHZ
@@ -33,43 +36,42 @@ class NearbyWifiAccessPointsApiImpl(
         delay(
             (updateTargetElapsedRealtimeNanos - SystemClock.elapsedRealtimeNanos()).nanoseconds - estimatedScanningDuration
         )
-        var scanResults: MutableList<ScanResult>? = null
-        var isScanning = true
-        val scanListener = object : ScanListener {
-            override fun onSuccess() {
-                Log.d(TAG, "onSuccess: ")
-            }
 
-            override fun onFailure(reason: Int, description: String?) {
-                Log.d(TAG, "onFailure: reason: $reason, description: $description")
-                isScanning = false
-            }
-
-            @Deprecated("Deprecated in Java")
-            override fun onPeriodChanged(periodInMs: Int) {
-                /* no-op */
-            }
-
-            override fun onResults(results: Array<out WifiScanner.ScanData>?) {
-                // For single scans, the array size should always be 1.
-                if (results?.size != 1) {
-                    Log.wtf(TAG, "Found more than 1 batch of scan results, Ignoring...")
-                    return
+        return suspendCancellableCoroutine { continuation ->
+            val scanListener = object : ScanListener {
+                override fun onSuccess() {
+                    Log.d(TAG, "onSuccess: ")
                 }
-                val scannedAccessPoints = results[0].results.toMutableList()
-                scanResults = scannedAccessPoints
-                isScanning = false
+
+                override fun onFailure(reason: Int, description: String?) {
+                    Log.d(TAG, "onFailure: reason: $reason, description: $description")
+                    continuation.resume(null)
+                }
+
+                @Deprecated("Deprecated in Java")
+                override fun onPeriodChanged(periodInMs: Int) {
+                    /* no-op */
+                }
+
+                override fun onResults(results: Array<out WifiScanner.ScanData>?) {
+                    // For single scans, the array size should always be 1.
+                    if (results?.size != 1) {
+                        Log.wtf(TAG, "Found more than 1 batch of scan results, Ignoring...")
+                        return
+                    }
+                    val scannedAccessPoints = results[0].results.toMutableList()
+                    continuation.resume(scannedAccessPoints)
+                }
+
+                override fun onFullResult(fullScanResult: ScanResult?) {
+                    /* no-op */
+                }
             }
 
-            override fun onFullResult(fullScanResult: ScanResult?) {
-                /* no-op */
-            }
+            wifiScanner.startScan(scanSettings, scanListener, workSource)
+
+            continuation.invokeOnCancellation { wifiScanner.stopScan(scanListener) }
         }
-        wifiScanner.startScan(scanSettings, scanListener, workSource)
-        while (isScanning) {
-            delay(100.milliseconds)
-        }
-        return scanResults
     }
 
     override fun setUpdateTarget(updateTargetElapsedRealtimeNanos: Long) {
